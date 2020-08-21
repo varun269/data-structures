18850687d7d6f7c4bd4463a9392381854adda5e7
varun269 <69524515+varun269@users.noreply.github.com>
2020-08-21 08:08:18 +0530

    dict.py
    deleting variables in the dictionary

---
+++ dict.py
@@ -0,0 +1,8 @@
+my_dict={
+    "blackhole":"gravity is so strong",
+    "wormhole":"shortcut in space",
+    "whitehole":"repulsive",
+    "milkyway galaxy":"where we live"
+}
+del my_dict["milkyway galaxy"]
+print(my_dict)
